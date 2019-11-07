c1222fd206a0f7fcf527f319f0c71428ee93307b
Clevin <Clevin.pro@gmail.com>
2019-11-07 12:30:43 +0200

    upd

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const dbPassword = '3bicFbMLwZn6Pnf5';
 const config = {
   secret: 'key123',
   port: 3000,
-  databaseUrl: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-gftfz.mongodb.net/shop?retryWrites=true&w=majority`
+  databaseUrl: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-gftfz.mongodb.net/nikeshop?retryWrites=true&w=majority`
 };
 
 module.exports = config;
